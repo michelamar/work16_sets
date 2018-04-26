e1bf81c35f016565882b029324c27ede2abf9e4e
andrewWong21 <awong21@stuy.edu>
2018-04-26 14:50:49 -0400

    started tests

--- sets.py
+++ sets.py
@@ -13,3 +13,8 @@ def symmetric_difference(l1, l2):
 def cartesian_product(l1, l2):
     return [(x, y) for x in l1 for y in l2]
 
+print "{1, 2, 3} u {2, 3, 4}:" + union({1, 2, 3}, {2, 3, 4})
+print "{1, 2, 3} n {2, 3, 4}:" + intersection({1, 2, 3}, {2, 3, 4})
+print "{1, 2, 3} / {2, 3, 4}:" + set_difference({1, 2, 3}, {2, 3, 4})
+print "{2, 3, 4} / {1, 2, 3}:" + set_difference({2, 3, 4}, {1, 2, 3})
+print "{1, 2, 3} u {2, 3, 4}:" + union({1, 2, 3}, {2, 3, 4})
